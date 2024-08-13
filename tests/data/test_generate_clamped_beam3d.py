a38b82e87b98b835e90a6d32d7923e1bb3ce1368
Sohei Arisaka <arisakas@kajima.com>
2024-08-13 10:01:17 +0000

    add generate_clamped_beam3d

---
+++ tests/data/test_generate_clamped_beam3d.py
@@ -0,0 +1,9 @@
+from nigbms.data.generate_clamed_beam3d import ClampedBeam3DParams, construct_petsc_clamped_beam3d
+from nigbms.modules.tasks import PETScLinearSystemTask
+
+
+# minimum test for construct_petsc_poisson2d_task
+def test_construct_petsc_clamped_beam3d():
+    parasm = ClampedBeam3DParams()
+    task = construct_petsc_clamped_beam3d(parasm)
+    assert isinstance(task, PETScLinearSystemTask)
--- tests/data/test_generate_poisson2d.py
+++ tests/data/test_generate_poisson2d.py
@@ -1,8 +1,9 @@
 from nigbms.data.generate_poisson2d import Poisson2DParams, construct_petsc_poisson2d_task
+from nigbms.modules.tasks import PETScLinearSystemTask
 
 
 # minimum test for construct_petsc_poisson2d_task
 def test_construct_petsc_poisson2d_task():
     parasm = Poisson2DParams()
     task = construct_petsc_poisson2d_task(parasm)
-    assert task is not None
+    assert isinstance(task, PETScLinearSystemTask)
