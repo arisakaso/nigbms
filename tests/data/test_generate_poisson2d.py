88fb353b27665961bfebe867c6fd4bc2bd15dd55
Sohei Arisaka <arisakas@kajima.com>
2024-08-15 09:03:55 +0000

    add None

--- tests/data/test_generate_poisson2d.py
+++ tests/data/test_generate_poisson2d.py
@@ -3,7 +3,7 @@ from nigbms.modules.tasks import PETScLinearSystemTask
 
 
 # minimum test for construct_petsc_poisson2d_task
-def test_construct_petsc_poisson2d_task():
+def test_construct_petsc_poisson2d_task() -> None:
     parasm = Poisson2DParams()
     task = construct_petsc_poisson2d_task(parasm)
     assert isinstance(task, PETScLinearSystemTask)
