b4bb58eb837f84ba14788354b3262ada31cc3401
Sohei Arisaka <arisakas@kajima.com>
2024-08-21 14:14:04 +0000

    add comment

--- tests/train/test_poisson1d.py
+++ tests/train/test_poisson1d.py
@@ -3,9 +3,10 @@ from hydra import compose, initialize
 from nigbms.train.poisson1d import main
 
 
+# this test may fail when running all tests at once. please run again separately if it fails.
 def test_main():
     with initialize(version_base="1.3", config_path="../configs/train"):
         cfg = compose(config_name="poisson1d")
-        cfg.wandb.project = "test"  # workaround for hydra error: raise ValueError("HydraConfig was not set"). see https://github.com/facebookresearch/hydra/issues/2017
+        cfg.wandb.project = "test_poisson1d"  # workaround for hydra error: raise ValueError("HydraConfig was not set"). see https://github.com/facebookresearch/hydra/issues/2017
         cfg.trainer.max_epochs = 1
         main(cfg)
