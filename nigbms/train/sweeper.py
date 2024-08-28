f9fef48d3ba681b3737148159fbb65183d5be59f
Sohei Arisaka <arisakas@kajima.com>
2024-08-28 09:11:06 +0000

    update sweeper

--- nigbms/train/sweeper.py
+++ nigbms/train/sweeper.py
@@ -11,16 +11,18 @@ import wandb
 
 @hydra.main(version_base="1.3", config_path="../configs/sweep", config_name="poisson1d_small")
 def main(cfg: DictConfig):
-    wandb.require("core")
-    cfg = OmegaConf.to_container(cfg, resolve=True)
-    cfg["command"] = ["${env}", "python", "${program}", "${args_no_hyphens}"]  # for hydra compatibility
-    sweep_id = wandb.sweep(cfg)
-    path = wandb.Api().sweep(sweep_id).path
-    path = os.path.join(*path)
-    # path = "sohei/poisson1d_small/u1bjp3xx"
-    agents = []
-    RUN_PER_GPU = 3
+    RUN_PER_GPU = 2
+    path = "sohei/poisson1d_small/9h7gji5c"
 
+    if path is None:
+        wandb.require("core")
+        cfg = OmegaConf.to_container(cfg, resolve=True)
+        cfg["command"] = ["${env}", "python", "${program}", "${args_no_hyphens}"]  # for hydra compatibility
+        sweep_id = wandb.sweep(cfg)
+        path = wandb.Api().sweep(sweep_id).path
+        path = os.path.join(*path)
+
+    agents = []
     for i in range(torch.cuda.device_count()):
         for _ in range(RUN_PER_GPU):
             agents.append(subprocess.Popen(f"CUDA_VISIBLE_DEVICES={i} wandb agent {path}", shell=True))
