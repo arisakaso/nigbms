c0493a6b45fdbc5a177dc8417b24cdf60ea74cd5
Sohei Arisaka <arisakas@kajima.com>
2024-09-27 02:14:58 +0000

    save

--- examples/minimize_testfunctions/train.py
+++ examples/minimize_testfunctions/train.py
@@ -2,19 +2,13 @@
 #       It should be easy to understand and should not contain any unnecessary code.
 #       lightning is not used in the example, so it should be removed.
 #       Also, consider removing the wandb logging later.
-import logging
 
 import hydra
-import lightning as pl
 import torch
-import wandb
 from hydra.utils import instantiate
 from nigbms.configs import constructors, meta_solvers, solvers, surrogates, wrapper  # noqa
 from nigbms.tasks import MinimizeTestFunctionTask
-from omegaconf import OmegaConf
 from torchinfo import summary
-
-log = logging.getLogger(__name__)
 
 
 #### TEST FUNCTIONS ####
@@ -46,22 +40,21 @@ def rastrigin(x) -> torch.Tensor:
 
 
 ### MAIN ###
-@hydra.main(version_base="1.3", config_path=".", config_name="minimize_testfunctions")
+@hydra.main(version_base="1.3", config_path=".", config_name="train")
 def main(cfg):
-    log.info(OmegaConf.to_yaml(cfg))
     # set up
-    wandb.config = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
-    wandb.init(project=cfg.wandb.project, config=wandb.config, mode=cfg.wandb.mode)
-    torch.set_default_dtype(eval(cfg.dtype))
-    torch.set_default_device(cfg.device)
-    pl.seed_everything(seed=cfg.seed, workers=True)
+    torch.set_default_dtype(torch.float64)
+    torch.set_default_device("cuda")
 
-    test_func = eval(cfg.problem.test_function)
+    test_func = sphere
     tau = MinimizeTestFunctionTask(None, test_func)
     meta_solver = instantiate(cfg.meta_solver)
-    solver = instantiate(cfg.solver)
+    solver = test_func
+    # instantiate(cfg.solver)
     surrogate = instantiate(cfg.surrogate)
     constructor = instantiate(cfg.constructor)
+    constructor = torch.nn.Identity()
+
     loss = torch.sum
     opt = instantiate(cfg.opt, params=list(meta_solver.parameters()))
     wrapped_solver = instantiate(cfg.wrapper, solver=solver, surrogate=surrogate, constructor=constructor)
@@ -82,10 +75,9 @@ def main(cfg):
         ref = theta.clone()  # copy to get the true gradient
         f_true = torch.autograd.grad(test_func(ref).sum(), ref)[0]
         sim = torch.cosine_similarity(f_true, theta.grad, dim=1, eps=1e-20).detach()
-        wandb.log({"ymean": y.mean(), "ymax": y.max(), "ymin": y.min(), "cos_sim": sim.mean()})
         if i % 100 == 0:
             surroate_loss = wrapped_solver.loss_dict["loss"]
-            log.info(f"{i=}, {y.mean()=:.3g}, {y.max()=:.3g}, {y.min()=:.3g}, {sim.mean()=:.3g}, {surroate_loss=:.3g}")
+            print(f"{i=}, {y.mean()=:.3g}, {y.max()=:.3g}, {y.min()=:.3g}, {sim.mean()=:.3g}, {surroate_loss=:.3g}")
 
         # clip gradients
         if cfg.clip:
--- nigbms/wrapper.py
+++ nigbms/wrapper.py
@@ -1,13 +1,33 @@
+from dataclasses import dataclass
+
 import pydevd  # noqa
 import torch
+from hydra.utils import instantiate
+from omegaconf import DictConfig
 from torch import Tensor, randn_like  # noqa
 from torch.autograd import Function, grad
-from hydra.utils import instantiate
-from nigbms.solvers import AbstractSolver
-from nigbms.data import PyTorchLinearSystemTask
-from nigbms.utils.solver import rademacher_like
+
 from nigbms.constructors import ThetaConstructor  # noqa
-from omegaconf import DictConfig
+from nigbms.data import PyTorchLinearSystemTask
+from nigbms.solvers import AbstractSolver
+from nigbms.utils.solver import rademacher_like
+
+
+@dataclass
+class NIGBMSConfig:
+    y: Tensor
+    f: callable
+    f_hat: torch.nn.Module
+    opt: torch.optim.Optimizer
+    loss: torch.nn.Module
+    v_dist: str
+    grad_type: str
+    jvp_type: str
+    eps: float
+    Nv: int
+    v_scale: float
+    clip: float
+    additional_steps: int
 
 
 class register_custom_grad(Function):
@@ -16,75 +36,74 @@ class register_custom_grad(Function):
     """
 
     @staticmethod
-    def forward(ctx, wrapper, theta):
+    def forward(ctx, theta, cfg: NIGBMSConfig):
         ctx.theta = theta
-        ctx.wrapper = wrapper
-        return wrapper.y
+        ctx.cfg = cfg
+        return cfg.y
 
     @staticmethod
     def backward(ctx, grad_y):
         # pydevd.settrace(suspend=False, trace_only_current_thread=True)  # for debugging
         theta = ctx.theta.detach().requires_grad_()
-        wrapper = ctx.wrapper
-        hparams = ctx.wrapper.hparams
+        cfg = ctx.cfg
 
         # placeholders
-        f_fwd = torch.zeros(hparams.Nv, *theta.shape, device=theta.device)
-        f_hat_true = torch.zeros(hparams.Nv, *theta.shape, device=theta.device)
-        cv_fwd = torch.zeros(hparams.Nv, *theta.shape, device=theta.device)
+        f_fwd = torch.zeros(cfg.Nv, *theta.shape, device=theta.device)
+        f_hat_true = torch.zeros(cfg.Nv, *theta.shape, device=theta.device)
+        cv_fwd = torch.zeros(cfg.Nv, *theta.shape, device=theta.device)
 
-        for i in range(hparams.Nv):
+        for i in range(cfg.Nv):
             with torch.no_grad():  # f is black-box
                 # sample random vector
-                if hparams.v_dist == "rademacher":
+                if cfg.v_dist == "rademacher":
                     v = rademacher_like(theta)
-                elif hparams.v_dist == "normal":
+                elif cfg.v_dist == "normal":
                     v = randn_like(theta)
                 else:
                     raise ValueError("v_dist must be 'rademacher' or 'normal'")
 
                 # compute forward gradient of the black-box solver (f)
-                if hparams.jvp_type == "forwardAD":
-                    _, dvf = torch.func.jvp(wrapper.f, (theta,), (v,))
-                elif hparams.jvp_type == "forwardFD":
-                    dvf = (wrapper.f(theta + v * hparams.eps) - wrapper.y) / hparams.eps
+                if cfg.jvp_type == "forwardAD":
+                    _, dvf = torch.func.jvp(cfg.f, (theta,), (v,))
+                elif cfg.jvp_type == "forwardFD":
+                    dvf = (cfg.f(theta + v * cfg.eps) - cfg.y) / cfg.eps
                 else:
                     raise ValueError("jvp_type must be 'forwardAD' or 'forwardFD")
 
                 dvL = torch.sum(grad_y * dvf, dim=1, keepdim=True)
                 f_fwd[i] = dvL * v
 
-            if hparams.grad_type in ["f_hat_true", "cv_fwd"]:
+            if cfg.grad_type in ["f_hat_true", "cv_fwd"]:
                 with torch.enable_grad():  # f_hat is differentiable
-                    wrapper.opt.zero_grad()
+                    cfg.opt.zero_grad()
 
                     # compute control forward gradient
-                    y_hat, dvf_hat = torch.func.jvp(wrapper.f_hat, (theta,), (v,))  # forward AD
+                    y_hat, dvf_hat = torch.func.jvp(cfg.f_hat, (theta,), (v,))  # forward AD
                     f_hat_true[i] = grad(y_hat, theta, grad_outputs=grad_y, retain_graph=True)[0]
                     dvL_hat = torch.sum(grad_y * dvf_hat, dim=1, keepdim=True)
                     f_hat_fwd = dvL_hat * v
-                    cv_fwd[i] = hparams.v_scale * (f_fwd[i] - f_hat_fwd) + f_hat_true[i]
+                    cv_fwd[i] = cfg.v_scale * (f_fwd[i] - f_hat_fwd) + f_hat_true[i]
 
                     # training surrogate
-                    parameters = [p for p in wrapper.surrogate.parameters() if p.requires_grad]
-                    wrapper.loss_dict = wrapper.loss(wrapper.y, y_hat, dvf, dvf_hat, dvL, dvL_hat)
-                    wrapper.loss_dict["loss"].backward(inputs=parameters)
-                    if isinstance(hparams.clip, float):
-                        torch.nn.utils.clip_grad_norm_(wrapper.surrogate.parameters(), hparams.clip)
-                    wrapper.opt.step()
+                    parameters = [p for p in cfg.surrogate.parameters() if p.requires_grad]
+                    cfg.loss_dict = cfg.loss(cfg.y, y_hat, dvf, dvf_hat, dvL, dvL_hat)
+                    cfg.loss_dict["loss"].backward(inputs=parameters)
+                    if isinstance(cfg.clip, float):
+                        torch.nn.utils.clip_grad_norm_(cfg.surrogate.parameters(), cfg.clip)
+                    cfg.opt.step()
 
-                    for _ in range(hparams.additional_steps):
-                        wrapper.opt.zero_grad()
-                        _y_hat, _dvf_hat = torch.func.jvp(wrapper.f_hat, (theta,), (v,))  # forward AD
+                    for _ in range(cfg.additional_steps):
+                        cfg.opt.zero_grad()
+                        _y_hat, _dvf_hat = torch.func.jvp(cfg.f_hat, (theta,), (v,))  # forward AD
                         _dvL_hat = torch.sum(grad_y * _dvf_hat, dim=1, keepdim=True)
-                        _loss_dict = wrapper.loss(wrapper.y, _y_hat, dvf, _dvf_hat, dvL, _dvL_hat)
+                        _loss_dict = cfg.loss(cfg.y, _y_hat, dvf, _dvf_hat, dvL, _dvL_hat)
                         _loss_dict["loss"].backward(inputs=parameters)
-                        if isinstance(hparams.clip, float):
-                            torch.nn.utils.clip_grad_norm_(wrapper.surrogate.parameters(), hparams.clip)
-                        wrapper.opt.step()
+                        if isinstance(cfg.clip, float):
+                            torch.nn.utils.clip_grad_norm_(cfg.surrogate.parameters(), cfg.clip)
+                        cfg.opt.step()
 
         with torch.no_grad():
-            grad_theta = torch.mean(eval(hparams.grad_type), dim=0)  # average over Nv
+            grad_theta = torch.mean(eval(cfg.grad_type), dim=0)  # average over Nv
 
         return None, grad_theta
 
